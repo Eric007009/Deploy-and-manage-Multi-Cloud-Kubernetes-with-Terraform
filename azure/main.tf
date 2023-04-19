3c45a392b1597fb743ed3e03c91caa53a8383668
JeffMa86 <98079741+JeffMa86@users.noreply.github.com>
2023-04-19 12:31:38 +0800

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -135,6 +135,50 @@ resource "azurerm_network_interface" "myterraformnic" {
   }
 }
 
+# Create resource group for sql server
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East Asia"
+}
+
+
+# Create sql server
+resource "azurerm_mssql_server" "example" {
+  name                         = "a06sqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "A06db"
+  administrator_login_password = "Mark199821!"
+
+  tags = {
+    environment = "production"
+  }
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "a06sqldatabase"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_database" "example" {
+  name           = "acctest-db-d"
+  server_id      = azurerm_mssql_server.example.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  read_scale     = false
+  sku_name       = "S0"
+  zone_redundant = false
+
+  tags = {
+    foo = "bar"
+  }
+}
+
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
   name                  = "myVM"
